2a8a118bec124cdf5b8a1285018213105ce6c4cb
Zeid Bsaibes <zeid.bsaibes@gmail.com>
2023-11-07 17:45:32 +0000

    add knex db config, run migrations, run seed, add instructions to readme

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 // Update with your config settings.
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
   client: "mysql2",
   connection: {
@@ -8,6 +11,5 @@ module.exports = {
     database: process.env.DB_LOCAL_DBNAME,
     user: process.env.DB_LOCAL_USER,
     password: process.env.DB_LOCAL_PASSWORD,
-    charset: "utf8",
   },
 };
