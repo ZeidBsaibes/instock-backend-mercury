e09377f9abb0e6ed2989478b5c97613c75828452
jamesc16 <jmacook16@gmail.com>
2023-11-07 16:56:32 +0000

    Knex inital setup

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+// Update with your config settings.
+
+module.exports = {
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_LOCAL_DBNAME,
+    user: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASSWORD,
+    charset: "utf8",
+  },
+};
