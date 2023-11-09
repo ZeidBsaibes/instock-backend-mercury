1cb7daca13d0227604d79590d93f09d755e846cf
Gemma Hudson <gemhud1@hotmail.com>
2023-11-09 20:41:44 +0000

    get newinventoryitem request working

--- controllers/inv-controller.js
+++ controllers/inv-controller.js
@@ -44,8 +44,56 @@ const getInventoryById = async (req, res) => {
 // create a single inventory item
 
 const createInventoryItem = async (req, res) => {
+  const { warehouse_id, item_name, description, category, status, quantity } =
+    req.body;
+
+  const newInventoryItem = {
+    warehouse_id: warehouse_id,
+    item_name: item_name,
+    description: description,
+    category: category,
+    status: status,
+    quantity: quantity,
+  };
+
+  if (
+    !warehouse_id ||
+    !item_name ||
+    !description ||
+    !category ||
+    !status ||
+    !quantity
+  ) {
+    return res
+      .status(400)
+      .send("Check for missing properties in request body:");
+  }
+
+  // const eligibleWarehouses = await knex("warehouses")
+  //   .select("id")
+  //   .where(eligibleWarehouses.id === newInventoryItem.warehouse_id);
+  // console.log(eligibleWarehouses);
+
+  const checkExist = await knex("warehouses")
+    .where({ id: warehouse_id })
+    .first();
+
+  if (!checkExist) {
+    return res
+      .status(400)
+      .send({ message: `warehouse with id ${req.body.id} not found` });
+  }
+
+  if (typeof quantity !== "number") {
+    return res.status(400).send("The quantity is the wrong data type:");
+  }
+
   try {
-    res.status(200).send("this is the route to create a new inventory item");
+    if (res.status === 400) {
+      return res.status(404).send("Other error");
+    }
+    const 
+    res.status(201).send("You have successfully created a new inventory item");
   } catch (error) {
     console.error(error);
   }
